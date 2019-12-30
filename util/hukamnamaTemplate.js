61a9d517db5af7662eeba9eb12d671c1e62820a7
Amandeep Singh <amandeepsingh.bajwa@gmail.com>
2019-12-30 09:38:36 +0530

    Update hukamnamaTemplate.js

--- util/hukamnamaTemplate.js
+++ util/hukamnamaTemplate.js
@@ -5,9 +5,10 @@ module.exports = `<!DOCTYPE html>
     <link rel="stylesheet" href="/style.css" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Daily Hukamnama from Sri Darbar Sahib Amritsar</title>
   </head>
   <body>
+    <!-- [DATE] -->
     <main id="hukamnama">[HUKUMNAMA]</main>
     <br>
     <a href="http://old.sgpc.net/hukumnama/jpeg%20hukamnama/hukamnama.mp3" target="_blank">Audio</a>
