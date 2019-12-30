a032ed0b9dbb8bb1a44ea79f6a81c638afbd540f
Amandeep Singh <amandeepsingh.bajwa@gmail.com>
2019-12-30 09:31:42 +0530

    Update Hukamnama.js

--- util/Hukamnama.js
+++ util/Hukamnama.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
 module.exports.getHukamHTML = async () => {
-  let response = await fetch(`https://api.gurbaninow.com/v2/hukamnama/today`);
+  let response = await fetch(`https://api.gurbaninow.com/v2/hukamnama/today${Math.random()*100000000000000000}`);
   let hukamnamaJSON = await response.json()
   let hukamnama = `<h3>${hukamnamaJSON.hukamnama[0].line.gurmukhi.unicode}</h3>`;
   hukamnamaJSON.hukamnama.shift();
