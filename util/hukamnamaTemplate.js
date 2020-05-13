dc0554f6add91b19fc8946b6cc2147693a591fd7
Amandeep Singh <amandeepsingh.bajwa@gmail.com>
2020-05-13 11:36:43 +0530

    Update hukamnamaTemplate.js

--- util/hukamnamaTemplate.js
+++ util/hukamnamaTemplate.js
@@ -1,7 +1,6 @@
 module.exports = `<!DOCTYPE html>
 <html>
   <head>
-    <title>A simple deployment with the Now API!</title>
     <link rel="stylesheet" href="/style.css" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
